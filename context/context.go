f8d5189574e6b747e68b784d71b9cf776905538d
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-04 10:54:14 -0500

    Removed logic for parsing config based on env's or path. This will be handled from the command line

--- context/context.go
+++ context/context.go
@@ -2,38 +2,20 @@ package context
 
 import (
 	"context"
-	"fmt"
-	"github.com/spf13/pflag"
+	config_model "github.com/stevezaluk/mtgjson-sdk/config"
 	"github.com/stevezaluk/mtgjson-sdk/server"
 )
 
 var ServerContext = context.Background()
 
-func InitConfig(flags *pflag.FlagSet) {
-	var config server.Config
-	env, err := flags.GetBool("env")
-	if err != nil {
-		fmt.Println("[error] Error with env flag: ", err)
-	}
-
-	if env {
-		config.ParseFromEnv()
-	} else {
-		configPath, err := flags.GetString("config")
-		if err != nil {
-			fmt.Println("[error] Error with config flag: ", err)
-		}
-
-		config.Parse(configPath)
-	}
-
+func InitConfig(config config_model.Config) {
 	ctx := context.WithValue(ServerContext, "config", config)
 	ServerContext = ctx
 }
 
 func InitDatabase() {
 	var database server.Database
-	database.Config = ServerContext.Value("config").(server.Config)
+	database.Config = ServerContext.Value("config").(config_model.Config)
 
 	database.Connect() // externalize errors to here instead of printing
 
