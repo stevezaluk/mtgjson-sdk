f583825e33d869272e0f7391275cab4ad74901d4
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-13 12:42:21 -0500

    Implemented functionality for fetching users from the database

--- user/user.go
+++ user/user.go
@@ -1,3 +1,25 @@
 package user
 
+import (
+	"github.com/stevezaluk/mtgjson-models/errors"
+	"github.com/stevezaluk/mtgjson-models/user"
+	"github.com/stevezaluk/mtgjson-sdk/context"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
+/*
+Fetch a user based on there username. Returns ErrNoUser if the user cannot be found
+*/
+func GetUser(username string) (user.User, error) {
+	var result user.User
+
+	var database = context.GetDatabase()
+
+	query := bson.M{"username": username}
+	results := database.Find("user", query, &result)
+	if results == nil {
+		return result, errors.ErrNoUser
+	}
+
+	return result, nil
+}
