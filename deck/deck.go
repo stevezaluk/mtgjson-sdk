f51f9c4b6fd29536d4408fedcdc30103c9d1ff3b
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-19 22:56:15 -0500

    Improved comments for all functions in deck.go

--- deck/deck.go
+++ deck/deck.go
@@ -11,52 +11,33 @@ import (
 )
 
 /*
-GetMainboard - Iterate through the UUID's in the main board and return card models
-
-Parameters:
-None
-
-Returns
-slice[card.Card] - The results
+Iterate through all the cards in the deck's mainboard and return a list of
+card models representing it
 */
 func GetMainboard(deck deck_model.Deck) []card_model.Card {
 	return card.GetCards(deck.Mainboard)
 }
 
 /*
-GetSideboard - Iterate through the UUID's in the side board and return card models
-
-Parameters:
-None
-
-Returns
-slice[card.Card] - The results
+Iterate through all the cards in the deck's sideboard and return a list of
+card models representing it
 */
 func GetSideboard(deck deck_model.Deck) []card_model.Card {
 	return card.GetCards(deck.Sideboard)
 }
 
 /*
-GetCommanders - Iterate through the UUID's in the commander board and return card models
-
-Parameters:
-None
-
-Returns
-slice[card.Card] - The results
+Iterate through all the cards in the deck's commander board and return a list of
+card models representing it
 */
 func GetCommanders(deck deck_model.Deck) []card_model.Card {
 	return card.GetCards(deck.Commander)
 }
 
 /*
-UpdateDeck - Replace the deck in the database
-
-Parameters:
-None
-
-Returns:
-error.ErrDeckUpdateFailed - If database.Replace returns an error
+Replace the entire deck in the database with the deck model
+passed in the parameter. Returns ErrDeckUpdateFailed if the deck
+cannot be located
 */
 func ReplaceDeck(deck deck_model.Deck) error {
 	var database = context.GetDatabase()
@@ -70,14 +51,9 @@ func ReplaceDeck(deck deck_model.Deck) error {
 }
 
 /*
-DeleteDeck - Delete the deck from the database
-
-Parameters:
-None
-
-Returns:
-errors.ErrNoDeck - If the deck does not exist
-errors.ErrDeckDeleteFailed - If the mongo results structure doesn't show any deleted results
+Remove a deck from the MongoDB database using the code passed in the
+parameter. Returns ErrNoDeck if the deck does not exist. Returns
+ErrDeckDeleteFailed if the deleted count does not equal 1
 */
 func DeleteDeck(code string) any {
 	var database = context.GetDatabase()
@@ -96,14 +72,8 @@ func DeleteDeck(code string) any {
 }
 
 /*
-GetDeck - Fetch a deck model and from a deck code
-
-Parameters:
-code (string) - The deck code
-
-Returns
-Deck (deck.Deck) - A deck model
-errors.ErrNoDeck - If the deck does not exist
+Fetch a deck from the MongoDB database using the code passed in the parameter. Returns
+ErrNoDeck if the deck does not exist or cannot be located
 */
 func GetDeck(code string) (deck_model.Deck, error) {
 	var result deck_model.Deck
@@ -120,13 +90,7 @@ func GetDeck(code string) (deck_model.Deck, error) {
 }
 
 /*
-GetDeckContents - Update the `contents` field in the passed deck model with the requested cards
-
-Parameters:
-deck (deck_model.Deck*) - A pointer to the deck you want to update
-
-Returns:
-None
+Updates the `contents` field in the passed deck model with the requested cards
 */
 func GetDeckContents(deck *deck_model.Deck) {
 	var mainBoard = deck.GetBoard(deck_model.MAINBOARD)
@@ -140,14 +104,8 @@ func GetDeckContents(deck *deck_model.Deck) {
 }
 
 /*
-IndexDecks - Fetch all decks available in the database
-
-Parameters:
-limit (int64) - Limit the ammount of results you want
-
-Returns:
-result (slice[deck.Deck]) - The results
-errors.ErrNoDecks - If no decks exist in the database
+Returns all decks in the database unmarshalled as deck models. The limit parameter
+will be passed directly to the database query to limit the number of models returned
 */
 func IndexDecks(limit int64) ([]deck_model.Deck, error) {
 	var result []deck_model.Deck
@@ -163,11 +121,8 @@ func IndexDecks(limit int64) ([]deck_model.Deck, error) {
 }
 
 /*
-NewDeck - Create a new deck from a deck model
-
-Parameters:
-errors.ErrDeskMissingId - If the deck passed in the parameter does not have a valid name or code
-errors.ErrDeckAlreadyExists - If the deck already exists under the same code
+Insert a new deck in the form of a model into the MongoDB database. The deck model must have a
+valid name and deck code, additionally the deck cannot already exist under the same deck code
 */
 func NewDeck(deck deck_model.Deck) error {
 	if deck.Name == "" || deck.Code == "" {
