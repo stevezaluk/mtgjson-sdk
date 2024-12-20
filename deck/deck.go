351b53caf84a7067d2ad3f0ce26b0655775e4f4c
Steven A. Zaluk <stevezaluk@gmail.com>
2024-12-20 14:36:01 -0500

    Updated NewDeck to take a pointer as a parameter

--- deck/deck.go
+++ deck/deck.go
@@ -3,36 +3,10 @@ package deck
 import (
 	"github.com/stevezaluk/mtgjson-sdk/context"
 
-	card_model "github.com/stevezaluk/mtgjson-models/card"
 	deck_model "github.com/stevezaluk/mtgjson-models/deck"
 	"github.com/stevezaluk/mtgjson-models/errors"
-	card "github.com/stevezaluk/mtgjson-sdk/card"
 	"go.mongodb.org/mongo-driver/bson"
 )
-
-/*
-Iterate through all the cards in the deck's mainboard and return a list of
-card models representing it
-*/
-func GetMainboard(deck deck_model.Deck) []card_model.Card {
-	return card.GetCards(deck.Mainboard)
-}
-
-/*
-Iterate through all the cards in the deck's sideboard and return a list of
-card models representing it
-*/
-func GetSideboard(deck deck_model.Deck) []card_model.Card {
-	return card.GetCards(deck.Sideboard)
-}
-
-/*
-Iterate through all the cards in the deck's commander board and return a list of
-card models representing it
-*/
-func GetCommanders(deck deck_model.Deck) []card_model.Card {
-	return card.GetCards(deck.Commander)
-}
 
 /*
 Replace the entire deck in the database with the deck model
@@ -90,20 +64,6 @@ func GetDeck(code string) (deck_model.Deck, error) {
 }
 
 /*
-Updates the `contents` field in the passed deck model with the requested cards
-*/
-func GetDeckContents(deck *deck_model.Deck) {
-	var mainBoard = deck.GetBoard(deck_model.MAINBOARD)
-	*mainBoard = append(*mainBoard, GetMainboard(*deck)...)
-
-	var sideBoard = deck.GetBoard(deck_model.SIDEBOARD)
-	*sideBoard = append(*sideBoard, GetSideboard(*deck)...)
-
-	var commanderBoard = deck.GetBoard(deck_model.COMMANDER)
-	*commanderBoard = append(*commanderBoard, GetCommanders(*deck)...)
-}
-
-/*
 Returns all decks in the database unmarshalled as deck models. The limit parameter
 will be passed directly to the database query to limit the number of models returned
 */
@@ -124,13 +84,13 @@ func IndexDecks(limit int64) ([]deck_model.Deck, error) {
 Insert a new deck in the form of a model into the MongoDB database. The deck model must have a
 valid name and deck code, additionally the deck cannot already exist under the same deck code
 */
-func NewDeck(deck deck_model.Deck) error {
+func NewDeck(deck *deck_model.Deck) error {
 	if deck.Name == "" || deck.Code == "" {
 		return errors.ErrDeckMissingId
 	}
 
-	_, valid := GetDeck(deck.Code)
-	if valid != errors.ErrNoDeck {
+	_, err := GetDeck(deck.Code)
+	if err != errors.ErrNoDeck {
 		return errors.ErrDeckAlreadyExists
 	}
 
