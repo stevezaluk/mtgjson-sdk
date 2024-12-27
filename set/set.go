0cfe0d61299315be5ef5cc04558e8566333909be
Steven A. Zaluk <stevezaluk@gmail.com>
2024-12-27 15:28:46 -0500

    Implemented models for creating a new set and fetching them under a specific owner

--- set/set.go
+++ set/set.go
@@ -1,7 +1,11 @@
 package set
 
 import (
+	"errors"
+	"github.com/stevezaluk/mtgjson-models/meta"
 	"github.com/stevezaluk/mtgjson-sdk/context"
+	"github.com/stevezaluk/mtgjson-sdk/user"
+	"github.com/stevezaluk/mtgjson-sdk/util"
 
 	sdkErrors "github.com/stevezaluk/mtgjson-models/errors"
 	"github.com/stevezaluk/mtgjson-models/set"
@@ -12,16 +16,67 @@ import (
 GetSet Takes a single string representing a set code and returns a set model for the set.
 Returns ErrNoSet if the set does not exist, or cannot be located
 */
-func GetSet(code string) (*set.Set, error) {
+func GetSet(code string, owner string) (*set.Set, error) {
 	var ret *set.Set
 	var database = context.GetDatabase()
 
-	err := database.Find("set", bson.M{"code": code}, &ret)
+	query := bson.M{"code": code}
+	if owner != "" {
+		query = bson.M{"code": code, "mtgjsonApiMeta.owner": owner}
+	}
+
+	err := database.Find("set", query, &ret)
 	if !err {
 		return ret, sdkErrors.ErrNoSet
 	}
 
 	return ret, nil
+}
+
+/*
+NewSet Insert a new set in the form of a model into the MongoDB database. The set model must have a
+valid name and set code, additionally the set cannot already exist under the same set code. Owner is
+the email address of the owner you want to assign the deck to. If the string is empty (i.e. == ""), it
+will be assigned to the system user
+*/
+func NewSet(set *set.Set, owner string) error {
+	if set.Name == "" || set.Code == "" {
+		return sdkErrors.ErrSetMissingId
+	}
+
+	if owner == "" {
+		owner = user.SystemUser
+	}
+
+	if owner != user.SystemUser {
+		_, err := user.GetUser(owner)
+		if err != nil {
+			return err
+		}
+	}
+
+	var database = context.GetDatabase()
+
+	_, err := GetSet(set.Code, owner)
+	if !errors.Is(err, sdkErrors.ErrNoSet) {
+		return sdkErrors.ErrSetAlreadyExists
+	}
+
+	currentDate := util.CreateTimestampStr()
+	if set.ReleaseDate == "" {
+		set.ReleaseDate = currentDate
+	}
+
+	set.MtgjsonApiMeta = &meta.MTGJSONAPIMeta{
+		Owner:        owner,
+		Type:         "Set",
+		CreationDate: currentDate,
+		ModifiedDate: currentDate,
+	}
+
+	database.Insert("set", &set)
+
+	return nil
 }
 
 /*
