649bb91f32ec7967a0dce0274a813ee3444d01c5
Steven A. Zaluk <stevezaluk@gmail.com>
2025-04-12 18:29:25 -0400

    Defined a basic abstraction of the server

--- server/server.go
+++ server/server.go
@@ -1,3 +1,9 @@
 package server
 
-
+/*
+Server - An abstraction of the Server running the MTGJSON-API. Includes any components
+necessary for functionality such as the Database, Logging, etc.
+*/
+type Server struct {
+	Database *Database
+}
