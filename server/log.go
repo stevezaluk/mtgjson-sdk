73284e9b60f97813ba5dabd83ead7105a10e0e0d
Steven A. Zaluk <stevezaluk@gmail.com>
2025-04-12 20:06:54 -0400

    Implemented a basic structure for the logger

--- server/log.go
+++ server/log.go
@@ -1,3 +1,20 @@
 package server
 
+import (
+	"log/slog"
+	"os"
+)
 
+/*
+Log - An abstraction of the slog logger used for the SDK
+*/
+type Log struct {
+	// logPath - The path that log files should be saved too
+	logPath string
+
+	// logFile - The file descriptor for the currently open log file
+	logFile *os.File
+
+	// logger - A pointer to the slog.Logger object being used by the logger
+	logger *slog.Logger
+}
